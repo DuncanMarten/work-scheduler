81e84a56450b34b4851824362fc43fba41bce1d4
Duncan <duncanmarten@yahoo.com>
2020-10-01 16:30:36 -0500

    created a template for the front page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,41 @@ setDay();
 
 var currentTime = moment().format("LT");
 
-var auditHour = function() {
+// var auditHour = function() {
+//     var hour = $(".container").find(".time").text().trim();
+//     var time = moment(hour, "LT");
     
-    var hour = $(".container").find(".time").text().trim();
-    var time = moment(hour, "LT");
+//     console.log(time);
+//     $(".time").removeClass("past present future");
 
-    console.log(time);
-    $(".time").removeClass("past present future");
+//     if (moment().isAfter(time)) {
+//         $(".description").addClass("past");
+//     }
+//     else if(moment().isBefore(time)) {
+//         $(".description").addClass("future");
+//     }
+//     else if(moment().is(time)) {
+//         $(".description").addClass("present");
+//     }
+// }
+// auditHour();
 
-    if (moment().isAfter(time)) {
-        $(".time").addClass("past");
-    }
-    else if(moment().isBefore(time)) {
-        $(".time").addClass("future");
-    }
-    else if(moment().is(time)) {
-        $(".time").addClass("present");
-    }
-}
-auditHour();
+
+
+
+// var list = JSON.parse(localStorage.getItem("tasklist")) || [];
+
+// function renderTasks(list) {
+
+// }
+
+
+
+
+
+// $(".saveBtn").on("click", function(event) {
+//     event.preventDefault();
+
+//     localStorage.setItem("tasklist", JSON.stringify(list));
+
+// });
