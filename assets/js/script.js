8d305e236820d308e9b6f8246f71a8c7c73cd74b
Duncan <duncanmarten@yahoo.com>
2020-10-02 14:33:06 -0500

    added a save to local storage and render from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,45 +10,72 @@ var currentTime = moment().format("LT");
 
 // update textarea to show if 
 var auditHour = function() {
-
+    // find all span
     var spanVal = document.querySelectorAll("span");
-
+    
+    // loop through spanVal array to find value of all span elements
     for (var i = 0; i < spanVal.length; i++) {
-        var hour = $(spanVal[i]).text();
-        console.log(hour);
-        $("textarea").removeClass("past present future");
+        // set hour to text content of each span element
+        var hour = $(spanVal[i]).text().trim();
+        //convert hour to a moment with 0 minutes
+        var time = moment(hour, "LT").local().set("minute", 00);
+        
+        // clear designated classes from textarea element
+        $(".task" + i).removeClass("past present future");
+    
 
-        if (moment().isAfter(hour)) {
-            $("textarea").addClass("past");
+        // assign .past if time has passed
+        if(moment().isAfter(time)) {
+            $(".task" + i).addClass("past");
         }
-        else if(moment().isBefore(hour)) {
-            $("textarea").addClass("future");
+
+        // assign .future if time is still to come
+        else if(moment().isBefore(time)) {
+            $(".task" + i).addClass("future");
         }
-        //else if(moment().is(spanVal)) {
-            //$("textarea").addClass("present");
-        //}
+
+        else if((Math.abs(Math.abs(moment().get("hour")-24)-12)).isSame(time)) {
+             $("textarea").addClass("present");
+        }
     }
 }
 auditHour();
 
-
-
-
-// var list = JSON.parse(localStorage.getItem("tasklist")) || [];
-
-// function renderTasks(list) {
-
-// }
-
-
-
-
-
-// $(".saveBtn").on("click", function(event) {
-//     event.preventDefault();
-
-//     localStorage.setItem("tasklist", JSON.stringify(list));
-
-// });
+// Render localstorage task list
+function renderTasks(timeTask) {
+    // pull and parse localStorage into tasks
+    var tasks = JSON.parse(localStorage.getItem("tasklist"));
+    
+    // Loop through each task array item and append it to designated task class
+    for (i = 0; i < tasks.length; i++) {
+        $(".task" + i).append(tasks[i]);
+    }
+}
+renderTasks();
 
 var timeTask = [];
+for (i = 0; i < 9; i++){
+$("#row" + i).on("click", "#btn" + i, function(event) {
+    event.preventDefault();
+    
+    var text0 = $(".task0").val();
+    var text1 = $(".task1").val();
+    var text2 = $(".task2").val();
+    var text3 = $(".task3").val();
+    var text4 = $(".task4").val();
+    var text5 = $(".task5").val();
+    var text6 = $(".task6").val();
+    var text7 = $(".task7").val();
+    var text8 = $(".task8").val();
+    
+    var tasks = [
+        text0, text1, text2, text3, text4, text5, text6, text7, text8
+    ]
+    console.log(tasks);
+    timeTask = tasks;
+    localStorage.setItem("tasklist", JSON.stringify(timeTask));
+});
+}
+
+
+// localStorage.setItem("tasklist", JSON.stringify(timeTask));
