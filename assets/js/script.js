9c281f5b2ce444a4537319fd312c6daf073252f4
Duncan <duncanmarten@yahoo.com>
2020-10-02 16:06:23 -0500

    fixed save and render to work more efficiently and fixed auditHour to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,26 @@
 // Set current date to html doc
 var setDay = function() {
+    // today's date
     var today = moment().format("dddd MMMM Do");
     $("#currentDay").append(today);
 }
-setDay();
 
-var currentTime = moment().format("LT");
-
+// arrays for task text and placement
+var timeTask = [];
+var placeTask = [];
 
 // update textarea to show if 
 var auditHour = function() {
+    
     // find all span
     var spanVal = document.querySelectorAll("span");
     
     // loop through spanVal array to find value of all span elements
     for (var i = 0; i < spanVal.length; i++) {
+        
         // set hour to text content of each span element
         var hour = $(spanVal[i]).text().trim();
+        
         //convert hour to a moment with 0 minutes
         var time = moment(hour, "LT").local().set("minute", 00);
         
@@ -34,48 +38,53 @@ var auditHour = function() {
             $(".task" + i).addClass("future");
         }
 
-        else if((Math.abs(Math.abs(moment().get("hour")-24)-12)).isSame(time)) {
+        else if(Math.abs(moment().diff(time, "hours")) <= 1) {
              $("textarea").addClass("present");
         }
     }
 }
-auditHour();
+
+
+// loop through each textarea element
+$("textarea").each(function(i) {
+    // for click on save button in designated row do ..
+    $("#row" + i).on("click", ".saveBtn", function(event){
+        
+        // set list to value of textarea
+        var list = $(".task" + i).val();
+        // set place to .task plus index
+        var place = ".task" + i;
+        
+        // push variables to arrays
+        timeTask.push(list);
+        placeTask.push(place);
+
+        // store arrays locally
+        localStorage.setItem("tasklist", JSON.stringify(timeTask));
+        localStorage.setItem("placement", JSON.stringify(placeTask));
+    })
+})
 
 // Render localstorage task list
 function renderTasks(timeTask) {
-    // pull and parse localStorage into tasks
-    var tasks = JSON.parse(localStorage.getItem("tasklist"));
     
-    // Loop through each task array item and append it to designated task class
-    for (i = 0; i < tasks.length; i++) {
-        $(".task" + i).append(tasks[i]);
+    // pull and parse localStorage into tasks
+    var timeTask = JSON.parse(localStorage.getItem("tasklist"));
+    var placeTask = JSON.parse(localStorage.getItem("placement"));
+    
+    // Loop through each task array item
+    for (i = 0; i < timeTask.length; i++) {
+        // append task text to placement id
+        $(placeTask[i]).append(timeTask[i]);
     }
 }
+
+// calling functions
+setDay();
+auditHour();
 renderTasks();
 
-var timeTask = [];
-for (i = 0; i < 9; i++){
-$("#row" + i).on("click", "#btn" + i, function(event) {
-    event.preventDefault();
-    
-    var text0 = $(".task0").val();
-    var text1 = $(".task1").val();
-    var text2 = $(".task2").val();
-    var text3 = $(".task3").val();
-    var text4 = $(".task4").val();
-    var text5 = $(".task5").val();
-    var text6 = $(".task6").val();
-    var text7 = $(".task7").val();
-    var text8 = $(".task8").val();
-    
-    var tasks = [
-        text0, text1, text2, text3, text4, text5, text6, text7, text8
-    ]
-    console.log(tasks);
-    timeTask = tasks;
-    localStorage.setItem("tasklist", JSON.stringify(timeTask));
-});
-}
-
-
-// localStorage.setItem("tasklist", JSON.stringify(timeTask));
+// every 15 minutes run auditHour function
+setInterval(function() {
+    auditHour();
+}, (15 * 60 * 1000));
